ce91b3ecb9d18ff5707575cef3f91b55ec25b307
Victoriano <fvictoriano@hrplabs.com>
2026-02-09 17:02:02 -0400

    [chore] try-catch util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function tryCatch<T>(
+  fn: (() => Promise<T>) | Promise<T>,
+): Promise<
+  { data: T; error: null } | { data: null; error: Error }
+> {
+  try {
+    let res: T;
+    if (typeof fn === "function") {
+      res = await fn();
+    } else {
+      res = await fn;
+    }
+    return { data: res, error: null };
+  } catch (err) {
+    return {
+      data: null,
+      error:
+        err instanceof Error ? err : Error(String(err)),
+    };
+  }
 }
